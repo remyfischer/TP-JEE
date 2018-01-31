b5e550c06ecdc307b3fbf434c167548ee6a1c759
remyfischer <remyfischer@live.fr>
2018-01-31 16:39:51 +0100

    modif finale

--- tpjee-ejb/src/main/java/entity/Vendeur.java
+++ tpjee-ejb/src/main/java/entity/Vendeur.java
@@ -1,18 +1,51 @@
 package entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 
 
 @Entity
 public class Vendeur extends Personne {
 	
-	
+	@OneToMany
+	private List<Article> articles;
 	public Vendeur(){
 		
 		super();
+		this.articles = new ArrayList<Article>();
 		
 		
-	}	
+		
+	}
+	
+	public void ajouterArticle(Article _article){
+		
+		this.articles.add(_article);
+		
+	}
+	
+	public List<Article> getArticles(){
+		
+		return this.articles;
+		
+	}
+	
+	public int getTotalArticleVendu(){
+		
+		int nbArticleVendu = 0;
+		for(Article article : this.getArticles()){
+			
+			nbArticleVendu++;
+			
+		}
+		
+		return nbArticleVendu;
+		
+	}
+
 	
 
 }
